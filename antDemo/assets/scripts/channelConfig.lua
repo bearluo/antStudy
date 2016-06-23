88235510fb5b44fe9abf24924577ab890bb58349
bearluo <BearLuo@boyaa.com>
2016-06-23 11:37:02 +0800

    antDemo 1.0.0

---
+++ antDemo/assets/scripts/channelConfig.lua
@@ -0,0 +1,14 @@
+------------php配置--------------
+if kPlatform == kPlatformIOS then
+    kAppid = PhpConfig.APPID_BOYAA
+    kAppkey = PhpConfig.APPKEY_BOYAA
+    kBid = PhpConfig.BID_BOYAA
+    kSid = PhpConfig.SID_IOS
+    kTypePar = PhpConfig.TYPE_YOUKE
+else
+    kAppid = PhpConfig.APPID_SLB
+    kAppkey = PhpConfig.APPKEY_SLB
+    kBid = PhpConfig.BID_SLB
+    kSid = PhpConfig.SID_GOOGLEPLAY
+    kTypePar = PhpConfig.TYPE_YOUKE
+end
---
+++ antDemo/build.bat
@@ -0,0 +1,6 @@
+@echo off
+echo #################渠道打包工具v1.0.0 by bearluo ################
+
+call ant auto-debug
+
+pause
